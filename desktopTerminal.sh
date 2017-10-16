1b52ea1e4bb23aaca4edaf892689fd978fa0201f
Taylor Martin <taylor.martin357@gmail.com>
2017-10-16 17:45:31 -0400

    Updated desktopTerminal

--- desktopTerminal.sh
+++ desktopTerminal.sh
@@ -3,8 +3,15 @@
 #https://www.reddit.com/r/mylittlelinux/comments/3s616r/embedded_terminal_shown_working_instructions_in/
 
 # xfce4-terminal --hide-borders --hide-toolbar --hide-menubar --title=desktopconsole --geometry=130x44+0+0 &
-xfce4-terminal --hide-borders --hide-toolbar --hide-menubar --title=desktopconsole --fullscreen &
-sleep 3
-wmctrl -r desktopconsole -b add,below,sticky
-wmctrl -r desktopconsole -b add,skip_taskbar,skip_pager
 
+if ! wmctrl -a desktopconsole ; then
+  xfce4-terminal --hide-borders --hide-toolbar --hide-menubar --title=desktopconsole --fullscreen &
+  sleep 1
+  wmctrl -r desktopconsole -b add,below,sticky
+  wmctrl -r desktopconsole -b add,skip_taskbar,skip_pager
+else 
+  #it is already running
+  wmctrl -r desktopconsole -b toggle,below
+  wmctrl -r desktopconsole -b toggle,skip_taskbar,skip_pager
+  wmctrl -a desktopconsole
+fi
