023efc753155c8a30612ab0e71054151416d2a41
Taylor Martin <taylor.martin357@gmail.com>
2017-10-17 20:35:51 -0400

    Updated terminal script Uses `--drop-down` now

--- desktopTerminal.sh
+++ desktopTerminal.sh
@@ -2,16 +2,4 @@
 
 #https://www.reddit.com/r/mylittlelinux/comments/3s616r/embedded_terminal_shown_working_instructions_in/
 
-# xfce4-terminal --hide-borders --hide-toolbar --hide-menubar --title=desktopconsole --geometry=130x44+0+0 &
-
-if ! wmctrl -a desktopconsole ; then
-  xfce4-terminal --hide-borders --hide-toolbar --hide-menubar --title=desktopconsole --fullscreen &
-  sleep 1
-  wmctrl -r desktopconsole -b add,below,sticky
-  wmctrl -r desktopconsole -b add,skip_taskbar,skip_pager
-else 
-  #it is already running
-  wmctrl -r desktopconsole -b toggle,below
-  wmctrl -r desktopconsole -b toggle,skip_taskbar,skip_pager
-  wmctrl -a desktopconsole
-fi
+xfce4-terminal --hide-borders --hide-toolbar --hide-menubar --title=desktopconsole --fullscreen --drop-down &
